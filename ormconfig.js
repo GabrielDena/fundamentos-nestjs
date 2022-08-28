136f7433d45b878510868a41fd7392a44c4aa6d9
Gabriel Dena <gabrieldenac@hotmail.com>
2022-08-28 20:54:18 -0300

    *Sections 2-3

---
+++ ormconfig.js
@@ -0,0 +1,10 @@
+module.exports = {
+	type: 'postgres',
+	host: 'localhost',
+	port: 5432,
+	username: 'postgres',
+	password: 'docker',
+	database: 'postgres',
+	entities: ['dist/**/entities/.entity.js'],
+	migrations: ['dist/migrations/*.js']
+}
